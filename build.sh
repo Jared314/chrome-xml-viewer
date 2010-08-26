882db9b23c36f4c441b4c3fb8d7442362ff0d846
Jared <pretender314@gmail.com>
2010-08-25 21:25:30 -0500

    added build script

---
+++ build.sh
@@ -0,0 +1,3 @@
+
+#java -jar compiler.jar --js etl.js etl.html.js etl.xml.js etl.transforms.js --js_output_file etl.compiled.js
+zip chrome-xml-viewer manifest.json background.html options.html document_end.1.js etl.js xml.reduced.firefox.css xml.reduced.standard.css xml.standard.firefox.css xml.standard.standard.css xregexp-min.js icon16.png icon19.png icon19_disabled.png icon32.png icon48.png icon128.png
